b5b07087a1db8d42104c94940a71f25ba547c7c1
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-04-23 12:08:25 +0700

    Updated test for Grid

--- src/components/Grid/Grid.test.js
+++ src/components/Grid/Grid.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import Grid from './Grid';
-import { initialState } from '../../store';
+import { COLUMNS, initialState, ROWS } from '../../store';
 
 test('Рендерится компонент', () => {
   render(<Grid matrix={initialState} />);
   const element = screen.getByTestId('grid');
   expect(element).toBeInTheDocument();
   expect(element).toBeVisible();
+  const elements = screen.getAllByTestId('cell');
+  let index = 0;
+  for (let rowIndex = 0; rowIndex < ROWS; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < COLUMNS; columnIndex++) {
+      expect(elements[(index * 9 + columnIndex)]).toHaveTextContent(initialState[rowIndex][columnIndex].value.toString())
+    }
+    index++;
+  }
 });
