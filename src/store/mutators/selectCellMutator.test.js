69dd9ae164beb5982924276c60c0b0736e1adfbf
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-05-13 12:08:53 +0700

    Added test for selectCellMutator

---
+++ src/store/mutators/selectCellMutator.test.js
@@ -0,0 +1,19 @@
+import { selectCellMutator } from './selectCellMutator';
+import { makeGrid } from '../gridCreator';
+import { COLUMNS, ROWS } from '../store';
+
+test('1 Мутатор получает на вход сетку и id возвращает сетку, в которой клетка с данным id имеет isChecked = true, а у всех остальных клеток false', () => {
+  const grid = makeGrid('secondPuzzle');
+  const id = 3;
+  grid[5][6].isChecked = true;
+  const newGrid = selectCellMutator(grid, id);
+  for (let rowIndex = 0; rowIndex < ROWS; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < COLUMNS; columnIndex++) {
+      if (newGrid[rowIndex][columnIndex].id === id) {
+        expect(newGrid[rowIndex][columnIndex].isChecked).toBe(true);
+      } else {
+        expect(newGrid[rowIndex][columnIndex].isChecked).toBe(false);
+      }
+    }
+  }
+});
