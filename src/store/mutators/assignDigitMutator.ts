906cc161487398d2634843473310df42066289cc
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-05-13 12:55:06 +0700

    Changed assignDigitMutator

--- src/store/mutators/assignDigitMutator.ts
+++ src/store/mutators/assignDigitMutator.ts
@@ -2,14 +2,11 @@ import { puzzles, ROWS, State } from '../store';
 
 export function assignDigitMutator(state: State, digit: number): State {
   let isError = false;
-  const newState = { ...state };
+  const newGrid = [...state.grid];
   for (let rowIndex = 0; rowIndex < ROWS; rowIndex++) {
-    newState.grid[rowIndex] = newState.grid[rowIndex].map((cell, cellIndex) => {
+    newGrid[rowIndex] = newGrid[rowIndex].map((cell, cellIndex) => {
       if (cell.isChecked && cell.isChangeable) {
-        if (
-          digit === puzzles[newState.currentPuzzle].solution[rowIndex][cellIndex] ||
-          digit === 0
-        ) {
+        if (digit === puzzles[state.currentPuzzle].solution[rowIndex][cellIndex] || digit === 0) {
           return {
             ...cell,
             value: digit,
@@ -28,7 +25,8 @@ export function assignDigitMutator(state: State, digit: number): State {
     });
   }
   return {
-    ...newState,
+    ...state,
+    grid: newGrid,
     errorCounter: isError ? state.errorCounter + 1 : state.errorCounter
   };
 }
