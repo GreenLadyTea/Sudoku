453ef86e7fd59cc18b967a6e179bad20ffd99150
kudinovamo <kudinovamo@mer.ci.nsu.ru>
2021-05-18 16:05:38 +0700

    Moved ErrorCounter, Menu and Grid to Puzzle Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
 import React from 'react';
 import './App.css';
-import Grid from './components/Grid/Grid';
-import Menu from './components/Menu/Menu';
-import ErrorCounter from './components/ErrorCounter/ErrorCounter';
-import { useSelector } from 'react-redux';
-import { ENDGAME_TYPES, MAXIMUM_AMOUNT_OF_ERRORS, State } from './store/store';
+import Puzzle from './components/Puzzle/Puzzle';
 
 export default function App() {
-  const isGameOver = useSelector((state: State) => state.gameIsOver);
   return (
     <>
-      {isGameOver === ENDGAME_TYPES.WIN && (
-        <>
-          <div className="win">ПОБЕДА!</div>
-        </>
-      )}
-      {isGameOver === ENDGAME_TYPES.FAIL && (
-        <>
-          <div className="fail">ПОРАЖЕНИЕ! Вы допустили {MAXIMUM_AMOUNT_OF_ERRORS} ошибок!</div>
-        </>
-      )}
-      {isGameOver === ENDGAME_TYPES.GAME_IS_NOT_OVER && (
-        <>
-          <div className="wrapper">
-            <h1>Судоку</h1>
-            <Grid />
-            <Menu />
-            <ErrorCounter />
-          </div>
-        </>
-      )}
+      <div className="wrapper">
+        <h1>Судоку</h1>
+        <Puzzle />
+      </div>
     </>
   );
 }
---
+++ src/components/Puzzle/Puzzle.tsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux';
+import { ENDGAME_TYPES, MAXIMUM_AMOUNT_OF_ERRORS, State } from '../../store/store';
+import Grid from '../Grid/Grid';
+import Menu from '../Menu/Menu';
+import ErrorCounter from '../ErrorCounter/ErrorCounter';
+import React from 'react';
+
+export default function Puzzle() {
+  const isGameOver = useSelector((state: State) => state.gameIsOver);
+  return (
+    <>
+      {isGameOver === ENDGAME_TYPES.WIN && (
+        <>
+          <div className="win">ПОБЕДА!</div>
+        </>
+      )}
+      {isGameOver === ENDGAME_TYPES.FAIL && (
+        <>
+          <div className="fail">ПОРАЖЕНИЕ! Вы допустили {MAXIMUM_AMOUNT_OF_ERRORS} ошибок!</div>
+        </>
+      )}
+      {isGameOver === ENDGAME_TYPES.GAME_IS_NOT_OVER && (
+        <>
+          <div>
+            <Grid />
+            <Menu />
+            <ErrorCounter />
+          </div>
+        </>
+      )}
+    </>
+  );
+}
