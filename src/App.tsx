03a6a07a6047a141a3abeb4150ae87cbedd46c64
GreenLadyTea <kudinovamo@mer.ci.nsu.ru>
2021-06-10 20:00:38 +0700

    Removed functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
-import Grid from './components/Grid/Grid';
-import { initialState } from './store';
 
-function App() {
-  return (
-    <div className="wrapper">
-      <h1>Судоку</h1>
-      <Grid matrix={initialState} />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>Судоку</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
